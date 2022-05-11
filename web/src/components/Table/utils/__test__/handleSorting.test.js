e9be6cd2fc08b901aa0501eed79c8208a04341b3
Nicholas3223 <nicholas.moschos@hotmail.com>
2022-05-11 13:44:37 +0200

    ascending and descending test cases added to handleSorting test

--- web/src/components/Table/utils/__test__/handleSorting.test.js
+++ web/src/components/Table/utils/__test__/handleSorting.test.js
@@ -1,88 +1,17 @@
 import handleSorting from '../handleSorting';
-
-const originalData = [
-  {
-    id: 1,
-    name: "Anfield",
-    metrics: {
-      volume: 1526,
-      margin: 20.65,
-      profit: 315
-    }
-  },
-  {
-    id: 2,
-    name: "Vicarage Road",
-    metrics: {
-      volume: 986,
-      margin: 17.99,
-      profit: 177
-    }
-  },
-  {
-    id: 3,
-    name: "Stamford Bridge",
-    metrics: {
-      volume: 2360,
-      margin: 25.88,
-      profit: 611
-    }
-  },
-  {
-    id: 4,
-    name: "Emirates Stadium",
-    metrics: {
-      volume: 1905,
-      margin: 17.08,
-      profit: 325
-    }
-  },
-];
-
-const nameSorted =  [
-  {
-    id: 1,
-    name: "Anfield",
-    metrics: {
-      volume: 1526,
-      margin: 20.65,
-      profit: 315
-    }
-  },
-  {
-    id: 4,
-    name: "Emirates Stadium",
-    metrics: {
-      volume: 1905,
-      margin: 17.08,
-      profit: 325
-    }
-  },
-  {
-    id: 3,
-    name: "Stamford Bridge",
-    metrics: {
-      volume: 2360,
-      margin: 25.88,
-      profit: 611
-    }
-  },
-  {
-    id: 2,
-    name: "Vicarage Road",
-    metrics: {
-      volume: 986,
-      margin: 17.99,
-      profit: 177
-    }
-  },
-];
+import { testData } from './testData';
 
 const setTableData = jest.fn();
 const setOrder = jest.fn();
 
-test('sorts data in order by name', () => {
-  handleSorting('name', 'string', originalData, 'asc', setTableData, setOrder);
-  expect(setTableData).toHaveBeenCalledWith(nameSorted);
+test('sorts data in order by name ascending', () => {
+  handleSorting('name', 'string', testData.originalData, 'asc', setTableData, setOrder);
+  expect(setTableData).toHaveBeenCalledWith(testData.nameSortedAsc);
   expect(setOrder).toHaveBeenCalledWith('dsc');
+});
+
+test('sorts data in order by name descending', () => {
+  handleSorting('name', 'string', testData.originalData, 'dsc', setTableData, setOrder);
+  expect(setTableData).toHaveBeenCalledWith(testData.nameSortedDsc);
+  expect(setOrder).toHaveBeenCalledWith('asc');
 });
