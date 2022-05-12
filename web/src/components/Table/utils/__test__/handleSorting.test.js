2fe62a464ebce68808fad2e5eec500f6b486a963
Nicholas3223 <nicholas.moschos@hotmail.com>
2022-05-12 09:44:26 +0200

    test cases added for handleSorting

--- web/src/components/Table/utils/__test__/handleSorting.test.js
+++ web/src/components/Table/utils/__test__/handleSorting.test.js
@@ -15,3 +15,15 @@ test('sorts data in order by name descending', () => {
   expect(setTableData).toHaveBeenCalledWith(testData.nameSortedDsc);
   expect(setOrder).toHaveBeenCalledWith('asc');
 });
+
+test('sorts data in order by profit ascending', () => {
+  handleSorting('profit', 'number', testData.originalData, 'asc', setTableData, setOrder);
+  expect(setTableData).toHaveBeenCalledWith(testData.profitSortedAsc);
+  expect(setOrder).toHaveBeenCalledWith('dsc');
+});
+
+test('sorts data in order by profit descending', () => {
+  handleSorting('profit', 'number', testData.originalData, 'dsc', setTableData, setOrder);
+  expect(setTableData).toHaveBeenCalledWith(testData.profitSortedDsc);
+  expect(setOrder).toHaveBeenCalledWith('asc');
+});
--- web/src/components/Table/utils/__test__/testData.js
+++ web/src/components/Table/utils/__test__/testData.js
@@ -113,6 +113,82 @@ const testData = {
       }
     },
   ],
+  profitSortedAsc: [
+    {
+      id: 2,
+      name: "Vicarage Road",
+      metrics: {
+        volume: 986,
+        margin: 17.99,
+        profit: 177
+      }
+    },
+    {
+      id: 1,
+      name: "Anfield",
+      metrics: {
+        volume: 1526,
+        margin: 20.65,
+        profit: 315
+      }
+    },
+    {
+      id: 4,
+      name: "Emirates Stadium",
+      metrics: {
+        volume: 1905,
+        margin: 17.08,
+        profit: 325
+      }
+    },
+    {
+      id: 3,
+      name: "Stamford Bridge",
+      metrics: {
+        volume: 2360,
+        margin: 25.88,
+        profit: 611
+      }
+    },
+  ],
+  profitSortedDsc: [
+    {
+      id: 3,
+      name: "Stamford Bridge",
+      metrics: {
+        volume: 2360,
+        margin: 25.88,
+        profit: 611
+      }
+    },
+    {
+      id: 4,
+      name: "Emirates Stadium",
+      metrics: {
+        volume: 1905,
+        margin: 17.08,
+        profit: 325
+      }
+    },
+    {
+      id: 1,
+      name: "Anfield",
+      metrics: {
+        volume: 1526,
+        margin: 20.65,
+        profit: 315
+      }
+    },
+    {
+      id: 2,
+      name: "Vicarage Road",
+      metrics: {
+        volume: 986,
+        margin: 17.99,
+        profit: 177
+      }
+    },
+  ]
 };
 
 export { testData };
